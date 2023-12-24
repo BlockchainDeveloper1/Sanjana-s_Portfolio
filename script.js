4fe45832384e3458f73f69a210279e54cf1611cb
Sanjana <149473371+BlockchainDeveloper1@users.noreply.github.com>
2023-12-24 21:03:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+
+
+var typed = new Typed('#element', {
+  strings: ['Blockchain Developer', 'Full Stack Blockchain Developer','Dapps Developer.', 'Solidity Developer','NFT Developer','Metaverse Developer','Blockchain Developer', 'Full Stack Blockchain Developer','Dapps Developer.', 'Solidity Developer','NFT Developer','Metaverse Developer'],
+  typeSpeed: 85
+});
+
